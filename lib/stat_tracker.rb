b352e9cdcab896233e1d1296a800567e60265c7d
Aurie <Myrdden42@gmail.com>
2019-04-01 20:12:03 -0600

    Add methods for Games to StatTracker

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,16 +1,24 @@
 require './lib/parse'
-require './lib/game'
-require './lib/team'
-require './lib/stat'
+require './lib/games'
+require './lib/teams'
+require './lib/stats'
 
 class StatTracker
   def initialize(locations)
     parser = ParseCSV.new if locations.values.all? {|x| File.extname(x) == ".csv"}
-    @game = Game.new(parser.parse(locations[:games]))
-    @team = Team.new(parser.parse(locations[:teams]))
-    @stat = Stat.new(parser.parse(locations[:stats]))
+    @games = Games.new(parser.parse(locations[:games]))
+    @teams = Teams.new(parser.parse(locations[:teams]))
+    @stats = Stat.new(parser.parse(locations[:stats]))
   end
 
-  def highest_total_score; return @game.highest_total_score end
+  def highest_total_score; return @games.highest_total_score end
+  def lowest_total_score; return @games.lowest_total_score end
+  def biggest_blowout; return @games.biggest_blowout end
+  def percentage_home_wins; return @games.percentage_home_wins end
+  def percentage_visitor_wins; return @games.percentage_visitor_wins end
+  def count_of_games_by_season; return @games.count_of_games_by_season end
+  def average_goals_per_game; return @games.average_goals_per_game end
+  def average_goals_by_season; return @games.average_goals_by_season end
+  
 
 end
