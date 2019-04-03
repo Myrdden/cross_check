3cb842403f350619745100eeeca5d52c4128cbde
davisc0801 <davisc1014@gmail.com>
2019-04-03 17:38:49 -0600

    Add hash patch for transform_keys
    Co-Authored-By: Aurie <Myrdden@users.noreply.github.com>

---
+++ lib/decor/hash_patch.rb
@@ -0,0 +1,10 @@
+class Hash
+  def transform_keys
+    return enum_for(:transform_keys) unless block_given?
+    result = self.class.new
+    each_key do |key|
+      result[yield(key)] = self[key]
+    end
+    result
+  end
+end
