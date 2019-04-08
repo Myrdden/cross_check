fd916f81aeea9391350ffc9a64089d1a65fc689f
davisc0801 <davisc1014@gmail.com>
2019-04-08 17:28:42 -0600

    Finish non-argument templates

--- site/ehtml_test.rb
+++ site/ehtml_test.rb
@@ -23,21 +23,48 @@ end
 
 template = %{
   <html>
-    <head><title>Hocky Sports Time Delux!!</title></head>
+    <head><title>Super Hocky Sports Time Deluxe!!</title></head>
     <body>
 
-      <h1>Hocky Sports Time Deluxe!!</h1>
+      <h1>Super Hocky Sports Time Deluxe!!</h1>
 
-      <p> Welcome to our hyper professional hockey stat page!
+      <p> Welcome to our hyper professional hockey stat page, eh!</p>
 
-      We used the NHL stats from the 2012 - 2013 to to 2017 - 2018 seasons to
-      generate our stats.
-
-      </p>
+      <p> We used the NHL stats from the 2012 - 2013 to to 2017 - 2018 seasons to
+      generate our stats.</p>
 
       <ul>
-          <li><b>This is the winningest team</b></li>
-          <li><b> <%= @stat_tracker.winningest_team %> </b></li>
+          <li>The highest total scoring game on record had
+          <%= @stat_tracker.highest_total_score %> goals.</li>
+          <li>The lowest total scoring game on record had
+          <%= @stat_tracker.lowest_total_score %> goals.</li>
+          <li>The biggest blowout was by <%= @stat_tracker.biggest_blowout %>
+          goals.</li>
+          <li>The percentage of home team wins was
+          <%= @stat_tracker.percentage_home_wins %> overall.</li>
+          <li>The percentage of visitor team wins was
+          <%= @stat_tracker.percentage_visitor_wins %> overall.</li>
+          <li>The average goals scored per game was
+          <%= @stat_tracker.average_goals_per_game %> overall.</li>
+          <li>The average goals scored per game by season were:
+          <%= @stat_tracker.average_goals_by_season %>.</li>
+          <li>There are <%= @stat_tracker.count_of_teams %> teams in the league </li>
+          <li>The team with the best overall offense is
+          <%= @stat_tracker.best_offense %></li>
+          <li>The team with the worst overall offense is
+          <%= @stat_tracker.worst_offense %></li>
+          <li>The team with the best overall defence is
+          <%= @stat_tracker.best_defense %></li>
+          <li>The team with the worst overall defence is
+          <%= @stat_tracker.worst_defense %></li>
+          <li>The team with the highest average score per game as
+          a visiting team is <%= @stat_tracker.highest_scoring_visitor %></li>
+          <li>The team with the highest average score per game as
+          the home team is <%= @stat_tracker.highest_scoring_home_team %></li>
+          <li>The team with the highest win percentage overall is
+          <%= @stat_tracker.highest_winningest_team %></li>
+          <li>The team with the best fans is
+          <%= @stat_tracker.highest_best_fans %></li>
       </ul>
 
       </p>
